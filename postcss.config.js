5278315732365bc300a6c0beefab149621bccd0c
Simon Peters <simonpeters05@gmail.com>
2021-03-13 16:36:11 +0100

    Initial commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,4 @@
+const tailwindcss = require("tailwindcss");
 module.exports = {
-    plugins: {
-        'postcss-import': {},
-        'tailwindcss': {},
-        'autoprefixer': {},
-        "postcss-preset-env":
-        {
-          autoprefixer: {
-            flexbox: "no-2009",
-          },
-          stage: 3,
-          features: {
-            "custom-properties": false,
-          },
-        },
-        'cssnano': process.env.NODE_ENV === 'production' ? {} : false
-    }
+  plugins: [tailwindcss("./tailwind.js"), require("autoprefixer")],
 };
