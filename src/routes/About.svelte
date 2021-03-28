c67d4f87c08e564acd54cf5c23c6105647c1856e
Simon Peters <simonpeters05@gmail.com>
2021-03-28 21:50:02 +0200

    Calling it a day

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -1,3 +1,20 @@
+<style scoped lang="postcss">
+div {
+  @apply inline-block;
+}
+#wrapper {
+  @apply w-full;
+}
+
+div#wrapper > div#content {
+  @apply block w-1/2 mx-auto;
+}
+</style>
+
 <script></script>
-<style></style>
-<h1>About</h1>
+
+<div id="wrapper">
+  <div id="content">
+    <h1>About</h1>
+  </div>
+</div>
--- src/routes/Gear.svelte
+++ src/routes/Gear.svelte
@@ -1,1 +1,20 @@
-<h1>Gear</h1>
+<style scoped lang="postcss">
+div {
+  @apply inline-block;
+}
+#wrapper {
+  @apply w-full;
+}
+
+div#wrapper > div#content {
+  @apply block w-1/2 mx-auto;
+}
+</style>
+
+<script></script>
+
+<div id="wrapper">
+  <div id="content">
+    <h1>Gear</h1>
+  </div>
+</div>
